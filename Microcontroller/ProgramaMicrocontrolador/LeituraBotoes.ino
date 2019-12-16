e2a817551596c2c870ea92ba92049cc14d4e4e5b
Elismar13 <elismarsilva.5@outlook.com>
2019-12-16 16:49:50 -0300

    Configurando o controle

---
+++ Microcontroller/ProgramaMicrocontrolador/LeituraBotoes.ino
@@ -0,0 +1,13 @@
+#define NUMBOTOESDIGITAIS    
+#define NUMBOTOESANALOGICOS
+
+typedef enum {
+  byte marcha;
+} Cambio;
+
+byte gerenciaMarcha(Cambio cambio) {
+
+}
+
+//Zero o conteudo do encoder
+if(!digitalRead(4)) Direcao.readAndReset();
